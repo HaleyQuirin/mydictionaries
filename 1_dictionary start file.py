be64116e5e42fc97da7aefe8fda4926e10d0ad84
HaleyQuirin <haley_quirin1@baylor.edu>
2023-01-30 14:11:58 -0600

    changes 1/30

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,15 +1,19 @@
 import random
 
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
+phonebook = {}
+phonebook = {"Chris": "555−1111", "Katie": "555−2222", "Joanne": "555−3333"}
 
 
+mydictionary = dict(m=8, n=9)
 
+print(mydictionary)
+
+print(f"Number of key-value pairs: {len(phonebook)}")
+
+"""
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
-
 
 
 
@@ -19,73 +23,42 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
 
 
 print()
-print('*****  start section 2 - search dictionary ********')
+print("*****  start section 2 - search dictionary ********")
 print()
 
+name = "chris"
 
-
-
-
-
-
-print()
-print('*****  end section 2 ********')
-print()
-
-
-
-
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(f"{name} does not exist in phone book")
 
 
 print()
-print('*****  start section 3 - edit/append dictionary ********')
-print()
-
-
-
-
-
-print()
-print('*****  end section 3 ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  start section 4 - delete/remove from dictionary ********')
+print("*****  end section 2 ********")
 print()
 
 
 
 
 print()
-print('*****  end section 4 ********')
+print("*****  start section 3 - edit/append dictionary ********")
 print()
 
 
+print(phonebook)
 
+phonebook["Chris"] = "555-4444"
 
+phonebook["Joe"] = "555-0123"
 
-
-print()
-print('*****  start section 5 - iterate through keys, values, items ********')
-print()
-
-
-
-
-
+print(phonebook)
 
 print()
-print('*****  end section 5 ********')
+print("*****  end section 3 ********")
 print()
 
 
@@ -93,52 +66,39 @@ print()
 
 
 print()
-print('*****  start section 6 - using get and clear ********')
+print("*****  start section 4 - delete/remove from dictionary ********")
+print()
+
+print(phonebook)
+del phonebook["Chris"]
+print(phonebook)
+
+
+print()
+print("*****  end section 4 ********")
 print()
 
 
 
 
-
-
 print()
-print('*****  end section 6 ********')
+print("*****  start section 5 - iterate through keys, values, items ********")
 print()
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is {phonebook[key]}")
 
+for value in phonebook.values():
+    print(value)
 
-print()
-print('*****  start section 7 - using pop method ********')
-print()
+for k, v in phonebook.items():
+    print(f"The key is: {k} and the value is {v}")
 
-
-
-
-
-
-print()
-print('*****  end section 7 ********')
-print()
-
-
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
 print()
-print('*****  start section 8 - using popitem ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 8 ********')
-print()
-
-
-
-print()
-print('*****  start section 9 - using random and converting to list ********')
+print("*****  end section 5 ********")
 print()
 
 
@@ -146,13 +106,68 @@ print()
 
 
 print()
-print('*****  end section 9 ********')
+print("*****  start section 6 - using get and clear ********")
+print()
+
+name = "Chris"
+phone = phonebook.get(name, "key not found")
+
+print(phone)
+
+phonebook.clear()
+
+print(phonebook)
+
+print()
+print("*****  end section 6 ********")
 print()
 
 
-'''
+print()
+print("*****  start section 7 - using pop method ********")
+print()
+
+remove = phonebook.pop("Chris", "not found")
+
+print(remove)
+print(phonebook)
+
+
+print()
+print("*****  end section 7 ********")
+print()
 
 
 
+print()
+print("*****  start section 8 - using popitem ********")
+print()
 
 
+a = phonebook.popitem()
+
+print(a)
+
+print(phonebook)
+
+
+print()
+print("*****  end section 8 ********")
+print()
+
+"""
+
+print()
+print("*****  start section 9 - using random and converting to list ********")
+print()
+
+list_of_keys = list(phonebook)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
+
+print(phonebook[random.choice(list(phonebook))])
+
+print()
+print("*****  end section 9 ********")
+print()
